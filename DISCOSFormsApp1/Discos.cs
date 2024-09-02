b7fff72c5a4bdb40ed7013d1360daa688a81f319
Agustin9830 <115587008+Agustin9830@users.noreply.github.com>
2024-09-02 17:34:24 -0300

    Nuevo proyecto agregado

---
+++ DISCOSFormsApp1/Discos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DISCOSFormsApp1
+{
+    internal class Discos
+    {
+        public string Titulo { get; set; }
+
+        public DateTime FechaLansamiento { get; set; }
+
+        public int CantidadCanciones { get; set; }
+    }
+}
---
+++ DISCOSFormsApp1/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace DISCOSFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDiscos = new System.Windows.Forms.DataGridView();
+            this.pbxDiscos = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDiscos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDiscos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDiscos
+            // 
+            this.dgvDiscos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDiscos.Location = new System.Drawing.Point(25, 32);
+            this.dgvDiscos.Name = "dgvDiscos";
+            this.dgvDiscos.Size = new System.Drawing.Size(469, 212);
+            this.dgvDiscos.TabIndex = 0;
+            // 
+            // pbxDiscos
+            // 
+            this.pbxDiscos.Location = new System.Drawing.Point(511, 34);
+            this.pbxDiscos.Name = "pbxDiscos";
+            this.pbxDiscos.Size = new System.Drawing.Size(277, 209);
+            this.pbxDiscos.TabIndex = 1;
+            this.pbxDiscos.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbxDiscos);
+            this.Controls.Add(this.dgvDiscos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDiscos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDiscos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDiscos;
+        private System.Windows.Forms.PictureBox pbxDiscos;
+    }
+}
+
